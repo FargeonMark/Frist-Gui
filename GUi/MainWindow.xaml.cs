e778573da7cce055faadb3b59207de57a1ab0446
FargeonMark <ederwinralph@gmail.com>
2021-11-10 09:06:15 +0530

    Add project files.

---
+++ GUi/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBlock Super = new TextBlock();
+            Super.Text = "Halo Guys, this is my first  ";
+            Super.TextWrapping = TextWrapping.Wrap;
+            this.Content = Super;
+        }
+    }
+}
